18a6af3e1d8f76e1d2e5225b2c57f8e327123a04
Marc Le Giteur <testgit@nodom.com>
2024-11-25 21:58:09 +0100

    function creation throw refactoring

--- app.py
+++ app.py
@@ -19,17 +19,48 @@ if "exercises_sql_tables.duckdb" not in os.listdir("data"):
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
+
+def check_users_solution(user_query: str) -> None:
+    """
+    Checks that user SQL query is correct by:
+    1: checking the columns
+    2: checking the values
+    :param user_query: a string containing the query inserted by the user
+    """
+    result = con.execute(user_query).df()
+    st.dataframe(result)
+    if len(result.columns) != len(
+        solution_df.columns
+    ):  # replace with try result = result[solution_df.columns]
+        st.write("Some columns are missing")
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError:
+        st.write("Some columns are missing")
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"Result has a {n_lines_difference} lines difference with the solution_df"
+        )
+
+
 with st.sidebar:
+    list_theme_df = con.execute("SELECT DISTINCT theme FROM memory_state").df()
     theme = st.selectbox(
         "What would you like to revise?",
-        ("cross_joins", "Group By", "Windows Functions"),
+        #         ("cross_joins", "Group By", "Windows Functions"),
+        list_theme_df["theme"].unique(),
         index=None,
         placeholder="Select a theme...",
     )
-    st.write("You selected:", theme)
-
+    if theme:
+        st.write("You selected:", theme)
+        SELECT_EXCERCISE_QUERY = f"SELECT * from memory_state WHERE theme = '{theme}'"
+    else:
+        SELECT_EXCERCISE_QUERY = "SELECT * from memory_state"
     exercise = (
-        con.execute(f"SELECT * from memory_state WHERE theme = '{theme}'")
+        con.execute(SELECT_EXCERCISE_QUERY)
         .df()
         .sort_values("last_reviewed")
         .reset_index()
@@ -45,26 +76,9 @@ with st.sidebar:
 
 st.header("Enter your code:")
 query = st.text_area(label="Your code here", key="user_input")
+
 if query:
-    result = con.execute(query).df()
-    st.dataframe(result)
-
-    if len(result.columns) != len(
-        solution_df.columns
-    ):  # replace with try result = result[solution_df.columns]
-        st.write("Some columns are missing")
-
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("Some columns are missing")
-
-    n_lines_difference = result.shape[0] - solution_df.shape[0]
-    if n_lines_difference != 0:
-        st.write(
-            f"Result has a {n_lines_difference} lines difference with the solution_df"
-        )
+    check_users_solution(query)
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
